0f2c8bba90fd0bceb3f748d31d252ad269a4db3c
Naga Brahma Chary <nagachary018@gmail.com>
2026-02-04 01:32:15 -0700

    updated props file

--- src/main/java/com/naga/github/TestToolService.java
+++ src/main/java/com/naga/github/TestToolService.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestToolService {
 
-    @McpTool
+    @McpTool(name="echo", description = "This will echo the input prompt")
     public String echo(String input) {
         return "Echo: " + input;
     }
 
-    @McpTool
+    @McpTool(name="add", description = "This will add two input int values, please prompt two int values to perform addition")
     public int add(int a, int b) {
         return a + b;
     }
