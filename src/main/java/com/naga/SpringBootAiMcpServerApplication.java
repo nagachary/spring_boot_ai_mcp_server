33c03360cb718e0f5ba74405321555350a0c1523
Naga Brahma Chary <nagachary018@gmail.com>
2026-02-03 22:48:40 -0700

    added server registration check

--- src/main/java/com/naga/SpringBootAiMcpServerApplication.java
+++ src/main/java/com/naga/SpringBootAiMcpServerApplication.java
@@ -1,12 +1,15 @@
 package com.naga;
 
 import com.naga.github.GitHubService;
+import io.modelcontextprotocol.server.McpServer;
+import io.modelcontextprotocol.server.McpSyncServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.support.ToolCallbacks;
 import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -27,6 +30,13 @@ public class SpringBootAiMcpServerApplication {
     @Bean
     public ToolCallbackProvider gitHubTools(GitHubService service) {
         return MethodToolCallbackProvider.builder().toolObjects(service).build();
+    }
+
+    @Bean
+    public ApplicationRunner checkMcpServer(McpSyncServer server) {
+        return args -> {
+            logger.info("MCP Server initialized at SSE endpoint: {}", server.getAsyncServer().getServerInfo().name());
+        };
     }
 
 }
